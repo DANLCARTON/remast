9b2f5b206e30ccd17db05e16453eedc33f4c38b1
Eric Thiberge <ericthiberge@orange.fr>
2024-03-15 00:57:23 +0100

    episode 5

--- src/Map.jsx
+++ src/Map.jsx
@@ -56,12 +56,70 @@ const QUERY_CREATE_POSTLIST = gql`
     }
 `
 
+const QUERY_CREATE_POST = gql`
+    mutation (
+        $latitude : Float!
+        $longitude : Float!
+        $content : String!
+    ) {
+        createPost (
+            data: {
+                latitude: $latitude,
+                longitude: $longitude,
+                content: $content
+            }
+        ) {
+            data {
+                id
+                attributes {
+                    latitude
+                    longitude
+                    content
+                }
+            }
+        }
+    }
+`
+
+const QUERY_UPDATE_POSTLIST = gql`
+    mutation (
+        $id : ID!
+        $posts : [ID]!
+    ) {
+        updatePostlist (
+            id: $id
+            data: {
+                posts: $posts
+            }
+        ) {
+            data {
+                id
+                attributes {
+                    posts {
+                        data {
+                            id
+                            attributes {
+                                latitude
+                                longitude
+                                content
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+`
+
 const Map = () => {
     const mapContainer = useRef(null)
     const map = useRef(null)
 
     const params = new URLSearchParams(window.location.search)
     const [id, setId] = useState(params.get("id"))
+
+    const [latitude, setLatitude] = useState(0)
+    const [longitude, setLongitude] = useState(0)
 
     const {loading, error, data : postlist} = useQuery(QUERY_GET_POSTLIST, {
         variables: {
@@ -72,11 +130,21 @@ const Map = () => {
     const posts = postlist?.postlist?.data?.attributes?.posts
 
     const [createPostlist] = useMutation(QUERY_CREATE_POSTLIST)
+    const [createPost] = useMutation(QUERY_CREATE_POST)
+    const [updatePostlist] = useMutation(QUERY_UPDATE_POSTLIST)
 
     useEffect(() => {
         map.current = new mapboxgl.Map({
             container: mapContainer.current,
             style: "mapbox://styles/mapbox/dark-v11"
+        })
+    }, [])
+
+    useEffect(() => {
+        map.current.on("move", () => {
+            const {lng, lat} = map.current.getCenter()
+            setLatitude(lat)
+            setLongitude(lng)
         })
     }, [])
 
@@ -101,7 +169,37 @@ const Map = () => {
         }
     }, [loading, postlist])
 
-    return <div ref={mapContainer} id="map" />
+    const handleSumbit = (e) => {
+        e.preventDefault()
+        createPost({
+            variables: {
+                latitude: latitude,
+                longitude: longitude,
+                content: e.target.content.value
+            }
+        }).then(data => {
+            const updatedPosts = [...posts.data, data.data.createPost.data]
+            const updatedPostsIds = []
+            updatedPosts.map(post => updatedPostsIds.push(parseInt(post.id)))
+            updatePostlist({
+                variables: {
+                    id: id,
+                    posts: updatedPostsIds
+                }
+            })
+        })
+    }
+
+    return <div className="Map">
+        <div id="form">
+            <form onSubmit={(e) => handleSumbit(e)}>
+                <p>{latitude} • {longitude}</p>
+                <textarea name="content" cols="30" rows="10"></textarea>
+                <input type="submit" value="poster ici" />
+            </form>
+        </div>
+        <div ref={mapContainer} className="map-container " id="map" />
+    </div>
 }
 
 export default Map
